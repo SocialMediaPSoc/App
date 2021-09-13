db72634e6afddc556c39d737963cf9a46c55ab41
AmishaGuddi <88842082+AmishaGuddi@users.noreply.github.com>
2021-09-14 03:14:12 +0530

    Update ProfilePage.dart

--- lib/pages/ProfilePage.dart
+++ lib/pages/ProfilePage.dart
@@ -1,16 +1,189 @@
+import 'package:buddiesgram/pages/EditProfilePage.dart';
+import 'package:buddiesgram/pages/HomePage.dart';
 import 'package:buddiesgram/widgets/HeaderWidget.dart';
+import 'package:buddiesgram/widgets/ProgressWidget.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:buddiesgram/models/user.dart';
 
 class ProfilePage extends StatefulWidget {
+  final String userProfileId;
+
+  ProfilePage({this.userProfileId});
+
+
+
+
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final String currentOnlineUserId = currentUser?.id;
+
+
+
+  createProfileTopView(){
+    return FutureBuilder(
+      future: usersReference.document(widget.userProfileId).get(),
+      builder: (context, dataSnapshot){
+        if(!dataSnapshot.hasData)
+        {
+          return circularProgress();
+        }
+        User user = User.fromDocument(dataSnapshot.data);
+        return Padding(
+          padding: EdgeInsets.all(17.0),
+          child: Column(
+            children: <widget>[
+              Row(
+                children: <widget>[
+                  CircleAvatar(
+                    radius: 45.0,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: CachedNetworkImageProvider(user.url),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: <widget>[
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <widget>[
+                            createColumns("posts",0),
+                            createColumns("followers",0),
+                            createColumns("following",0),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <widget>[
+                            creatButton(),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+              Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(top: 13.0),
+                child: Text(
+                  user.username, style: TextStyle(fontSize: 14.0, color: Colors.white),
+                ),
+              ),
+               
+               Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(top: 5.0),
+                child: Text(
+                  user.profileName, style: TextStyle(fontSize: 18.0, color: Colors.white),
+                ),
+              ),
+               
+               Container(
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.only(top: 3.0),
+                child: Text(
+                  user.bio, style: TextStyle(fontSize: 18.0, color: Colors.white70),
+                ),
+              ),
+               
+            ],
+          ),
+          
+          
+          );
+
+      },
+      );
+  }
+
+
+  Column createColumns(String title, int count){
+    return Column(
+     mainAxisSize: MainAxisSize.min,
+     mainAxisAlignment: MainAxisAlignment.center,
+     children: <widget>[
+       Text(
+         count.toString(),
+         style: TextStyle(fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold),
+
+
+       ),
+
+       Container(
+         margin: EdgeInsets.only(top: 5.0),
+         child: Text(
+           title,
+           style: TextStyle(fontSize: 16.0, color: Colors.grey, fontWeight: FontWeight.w400),
+         ),
+       ),
+       
+     ],
+
+
+    );
+  }
+
+  creatButton(){
+    bool ownProfile = currentOnlineUserId == widget.userProfileId;
+    if(ownProfile){
+      return creatButtonTitleAndFunction(title: "Edit Profile", performFuntion:editUserProfile,);
+    }
+  }
+
+  creatButtonTitleAndFunction({String title, Function performFunction}){
+    return Container(
+      padding: EdgeInsets.only(top: 3.0),
+      child: FlatButton(
+        onPressed: performFunction,
+        child: Container(
+          width: 245.0,
+          height: 26.0,
+          child: Text(title, style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold ),),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(6.0),
+            ),
+        ),
+        ),
+
+
+    );
+  }
+
+
+  editUserProfile(){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfilePage(currentOnlineUserId: currentOnlineUserId)));
+  }
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: header(context, strTitle: "Profile", ),
+body: ListView(
+  children: <Widget> [
+    createProfileTopView(),
+
+  ],
+),
+
     );
   }
+}
+
+class widget {
 }
